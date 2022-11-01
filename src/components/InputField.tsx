592169b4f5212f5dae4be8d96cb51b03cf0c00c7
Iosif <screpy@iosifs-air.home>
2022-11-01 18:26:33 +0000

    add state

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,7 +5,7 @@ const InputField = () => {
   return (
     <form className='input'>
       <input className='input__box' type="text" placeholder='Enter a task' />
-      <button className='input__submit' type='submit'>Submit</button>
+      <button className='input__submit' type='submit'>Go</button>
     </form>
   )
 }
