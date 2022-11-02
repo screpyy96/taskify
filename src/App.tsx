f1222db40dc86469557777b1c248b65e949b3ef6
Iosif <screpy@iosifs-air.home>
2022-11-02 00:00:38 +0000

    add Todo Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField'
+import TodoList from './components/TodoList';
+import { Todo } from './Model';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('')
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), isDone: false, todo }])
+      setTodo('')
+    }
+  }
+
+  console.log(todos)
   return (
     <div className="app">
       <span className='heading'>Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './styles.css'
 
-const InputField = () => {
+interface Props {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>,
+  handleAdd: (e: React.FormEvent) => void
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
   return (
-    <form className='input'>
-      <input className='input__box' type="text" placeholder='Enter a task' />
+    <form className='input' onSubmit={(e) => {
+      handleAdd(e)
+      inputRef.current?.blur()
+    }}>
+      <input className='input__box'
+        ref={inputRef}
+        value={todo}
+        onChange={
+          (e) => setTodo(e.target.value)
+        }
+        type="text" placeholder='Enter a task' />
       <button className='input__submit' type='submit'>Go</button>
     </form>
   )
